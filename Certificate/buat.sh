0757867f7ac8b434480e707ce8517b4b4222bef1
winardiaris <ariswinardy@gmail.com>
2015-04-09 14:47:50 +0700

    certificate

---
+++ Certificate/buat.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+  rm -rf "pdf/*"
+  SAVEIF=$IFS
+  IFS=$(echo -en "\n\b")
+  for file in $(cat data.csv)
+  do
+   data=${file%%}
+	#echo  "+$data"
+		IFS=',' read -a array <<< "$data"
+		
+			cp "master.svg" "pdf/${array[0]}.svg"
+			replace "&lt;&lt;NAME&gt;&gt;" "${array[1]} ${array[2]}" -- "pdf/${array[0]}.svg"
+			replace "&lt;&lt;KODE&gt;&gt;" "${array[0]}" -- "pdf/${array[0]}.svg"
+			
+			inkscape --export-pdf="pdf/${array[0]}.pdf" "pdf/${array[0]}.svg"
+
+			#echo "ID: ${array[0]}"
+			#echo "First: ${array[1]}"
+			#echo "Last: ${array[2]}"
+
+  done
+  cd "pdf/"
+  rm *.svg
+  pdfunite *.pdf all.pdf
